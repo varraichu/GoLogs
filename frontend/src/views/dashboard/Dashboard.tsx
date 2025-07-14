57510f371bf9c90bf5a136f996c80dc464923f5b
varraichu <136586057+varraichu@users.noreply.github.com>
2025-07-14 17:09:46 +0500

    fixed merge error

--- frontend/src/views/dashboard/Dashboard.tsx
+++ frontend/src/views/dashboard/Dashboard.tsx
@@ -12,7 +12,7 @@ import { PinUnpinDialog, handleCheckboxChange, savePinnedApps } from "./componen
 
 import dashboardService, { Application } from "../../services/dashboard.services";
 
-const Dashboard = (props: { path?: string; userId?: string, setActiveItem:(str:string)=>void }) => {
+const Dashboard = (props: { path?: string; userId?: string, setActiveItem: (str: string) => void }) => {
     const [applications, setApplications] = useState<Application[]>([]);
     const [userId, setUserId] = useState("");
     const [showPinDialog, setShowPinDialog] = useState(false);
@@ -49,7 +49,7 @@ const Dashboard = (props: { path?: string; userId?: string, setActiveItem:(str:s
                 </div>
             </div>
             <div class="oj-flex-item oj-sm-margin-4x-bottom">
-                <AppsHealth userId={userId} setActiveItem={props.setActiveItem}/>
+                <AppsHealth userId={userId} setActiveItem={props.setActiveItem} />
             </div>
 
             {/* Logs Graph & Pinned Apps */}
@@ -88,14 +88,13 @@ const Dashboard = (props: { path?: string; userId?: string, setActiveItem:(str:s
 
 
             {/* Recent Logs */}
-
             <div
                 class="oj-panel oj-panel-shadow-xs oj-sm-padding-4x"
                 style={{ height: '100%', display: 'flex', flexDirection: 'column' }}
             >
-
-                <DashboardRecentLogs />
+                <DashboardRecentLogs setActiveItem={props.setActiveItem} />
             </div>
+
 
             {/* Error Dialog */}
             {showErrorDialog && (
